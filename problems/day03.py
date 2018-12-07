7fd986ef35b5acd38eda020b59a7d72c492d288a
Joshua Mellott-Lillie <joshua.mellott@accuweather.com>
2018-12-06 21:20:07 -0500

    Day3: Part2
    * Added a claim class for day 3
    * Solved Part 2

--- problems/day03.py
+++ problems/day03.py
@@ -1,9 +1,31 @@
 """"Day 3: No Matter How You Slice It
 https://adventofcode.com/2018/day/3
 Part 1: How many square inches of fabric are within two or more claims?
+Part 2: What is the ID of the only claim that doesn't overlap?
 """
 
 import re
+
+class Claim:
+    pattern = re.compile('#\\d+ @ (\\d+),(\\d+): (\\d+)x(\\d+)')
+
+    def __init__(self, input):
+        match = Claim.pattern.match(input)
+        self.line = input
+        self.x = int(match.group(1))
+        self.y = int(match.group(2))
+        self.x2 = self.x + int(match.group(3))
+        self.y2 = self.y + int(match.group(4))
+    
+    def overlaps(self, other):
+        if self.x > other.x2 or self.y > other.y2:
+            return False
+        if self.x2 < other.x or self.y2 < other.y:
+            return False
+        return True
+
+    def overlaps_area(self, other):
+        return (min(self.x2, other.x2) - max(self.x, other.x)) * (min(self.y2, other.y2) - max(self.y, other.y))
 
 def _load_file():
     # Open the file in read-only mode
@@ -20,23 +42,17 @@ def _part1():
     """
     # Load the input
     f = _load_file()
-    # Generate pattern for the input data
-    pattern = re.compile('#\\d+ @ (\\d+),(\\d+): (\\d+)x(\\d+)')
+    # map the claims to objects
+    claims = map(lambda line: Claim(line), f)
     # Specify the maximum fabric dimensions
     N = 1000
     # Initialize an array for the fabric
     fabric = [0] * (N * N)
     # For each claim
-    for line in f:
-        # Regex it & Extract Values
-        match = pattern.match(line)
-        x = int(match.group(1))
-        y = int(match.group(2))
-        w = int(match.group(3))
-        h = int(match.group(4))
+    for claim in claims:
         # Fill the fabric values
-        for i in range(x, x + w):
-            for j in range(y, y + h):
+        for i in range(claim.x, claim.x2):
+            for j in range(claim.y, claim.y2):
                 fabric[i * N + j] += 1
     # Sum the squares with more than 1 claim
     overlapping = 0
@@ -44,8 +60,40 @@ def _part1():
         for i in range(N):
             if fabric[i * N + j] > 1:
                 overlapping += 1
-    
     return overlapping
 
+def _part2():
+    """Part 2
+    Use regex to parse the input (claims).
+    Map the claims to the Claim class for easier usage
+    Check each claim against each other claim
+    Returns:
+        String: Corresponding line of the claim
+    """
+    # load the input
+    f = _load_file()
+    # map the claims to objects
+    claims = list(map(lambda line: Claim(line), f))
+    # create a list of unique claims
+    unique = list()
+    # go through each claim
+    for claim in claims:
+        # make a list of claims to remove if deemed no longer unique
+        toremove = list()
+        # test against all previously found unique claims
+        for uniqueClaim in unique:
+            # check if the claim overlaps with any unique
+            if claim.overlaps(uniqueClaim):
+                # if they do overlap, neither is unique
+                toremove.append(uniqueClaim)
+        # remove any claims found to not be unique
+        for r in toremove:
+            unique.remove(r)
+        # if no claims were found that were unique (so far) then this one is
+        if len(toremove) == 0:
+            unique.append(claim)
+    # I'm just gonna assume there is one here, YOLO
+    return unique[0].line
+
 def main():
-    return _part1()
+    return _part1(), _part2()
